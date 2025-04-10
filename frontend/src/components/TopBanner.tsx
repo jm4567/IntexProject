fb7086915c2717b9b155aa8c969554d9f3e22e5e
racserre <153023119+racserre@users.noreply.github.com>
2025-04-10 12:31:00 -0600

    Endless Scrolling

---
+++ frontend/src/components/TopBanner.tsx
@@ -0,0 +1,20 @@
+// File: components/TopBanner.tsx
+import React from 'react';
+import { Movie } from '../types/Movie';
+import Header from './Header';
+
+interface TopBannerProps {
+  movies: Movie[];
+}
+
+const TopBanner: React.FC<TopBannerProps> = ({ movies }) => {
+  const topBannerMovies = movies.filter((movie) =>
+    ['s42', 's7073', 's603', 's6065', 's6891', 's6063', 's6152'].includes(
+      movie.showId
+    )
+  );
+
+  return <Header movies={topBannerMovies} />;
+};
+
+export default TopBanner;
